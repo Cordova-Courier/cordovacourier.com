8555a5f5fa2c1c433dec5c62bfc055db5125226c
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-05-01 12:58:34 -0700

    Update header-scroll.js

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -1,10 +1,8 @@
 window.addEventListener('scroll', function () {
   const header = document.getElementById('site-header');
-  if (window.scrollY > 60) {
+  if (window.scrollY > 50) {
     header.classList.add('scrolled');
-    header.classList.remove('transparent');
   } else {
     header.classList.remove('scrolled');
-    header.classList.add('transparent');
   }
 });
