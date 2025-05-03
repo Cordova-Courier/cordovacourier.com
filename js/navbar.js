7e767357378521519842d146d1531d7c39523a56
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-05-03 10:28:35 -0700

    Create navbar.js

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 40) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+function toggleMobileMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
