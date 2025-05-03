a22f8f65924395834fb045b60b3e9732fa731211
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-05-03 11:24:12 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -9,5 +9,5 @@ window.addEventListener("scroll", function () {
 
 function toggleMobileMenu() {
   const menu = document.getElementById("mobileMenu");
-  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+  menu.classList.toggle("show");
 }
