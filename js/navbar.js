3c3809475d45a092cce16baf2323b3c70cec7a1e
Vinnie <85443832+vincecordova@users.noreply.github.com>
2025-05-11 15:09:42 -0700

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -20,19 +20,45 @@ document.addEventListener("DOMContentLoaded", function () {
       if (window.scrollY > 5) {
         navbar.classList.add("scrolled");
         console.log("Navbar scrolled on");
+
         if (logo) {
           logo.src = "/images/branding/cordova-navbar-logo.png";
           logo.style.height = "40px";
           logo.style.position = "static";
         }
+
+        if (navLinks) {
+          navLinks.innerHTML = `
+            <li><a href="/">Home</a></li>
+            <li><a href="/quote/">Request a Quote</a></li>
+            <li><a href="/services/">Services</a></li>
+            <li><a href="/service-areas/">Service Areas</a></li>
+            <li><a href="/about-us/">About Us</a></li>
+            <li><a href="/tracking/">Tracking</a></li>
+            <li><a class="cta-btn" href="https://secure.ontime360.com/sites/CordovaCourierLLC/" target="_blank">Customer Portal</a></li>
+          `;
+        }
       } else {
         navbar.classList.remove("scrolled");
         console.log("Navbar scrolled off");
+
         if (logo) {
           logo.src = "/images/branding/cordova-logo.png";
           logo.style.height = "140px";
           logo.style.position = "absolute";
           logo.style.top = "-40px";
+        }
+
+        if (navLinks) {
+          navLinks.innerHTML = `
+            <li><a href="/">Home</a></li>
+            <li><a href="/quote/">Request a Quote</a></li>
+            <li><a href="/services/">Services</a></li>
+            <li><a href="/service-areas/">Service Areas</a></li>
+            <li><a href="/about-us/">About Us</a></li>
+            <li><a href="/tracking/">Tracking</a></li>
+            <li><a class="cta-btn" href="https://secure.ontime360.com/sites/CordovaCourierLLC/" target="_blank">Customer Portal</a></li>
+          `;
         }
       }
     });
